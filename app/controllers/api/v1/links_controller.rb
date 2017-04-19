3747adf86e2a54bdf4387036d13b046e1096376c
Amz K <akintner@u.rochester.edu>
2017-04-19 11:06:54 -0600

    add create route for links_controller, all tests passing

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::LinksController < BaseController
   def index
     @links = Link.top_ten
+    render json: @links
+  end
+
+  def create
+    @link = Link.find_or_initialize_by(link_params)
+    @link.read_count += 1
+    if @link.save
+      render json: @link
+    else
+      render status: 404
+    end
   end
 
 
